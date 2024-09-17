4fe9a345c50d16f33c17aa55232ca7e2354b2d09
Breno L. Lopes <brernolopes@hotmail.com>
2024-09-17 14:58:47 -0300

    feat: :sparkles: bmiCalculation
    Implementando a função bmiCalculation

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ export const convertTemperature = (type, temperature) => {
     const toCelcius = (value - 32) / 1.8;
     return toCelcius;
 };
+
+export const bmiCalculation = (weight, height) => {
+    if (isNaN(weight) || isNaN(height)) {
+        throw new Error('Os valores devem ser um número');
+    }
+
+    const calculation = weight / (height * height);
+
+    if (calculation < 18.5) {
+        return 'Abaixo do peso normal';
+    } else if (calculation >= 18.5 && calculation <= 24.9) {
+        return 'Normal';
+    } else if (calculation >= 25.0 && calculation <= 29.9) {
+        return 'Sobrepeso';
+    } else if (calculation >= 30.0 && calculation <= 34.9) {
+        return 'Obesidade grau I';
+    } else if (calculation >= 35.0 && calculation <= 39.9) {
+        return 'Obesidade grau II';
+    } else {
+        return 'Obesidade grau III';
+    }
+};
